83544ea7ab823168ef777733714b7e2aa3740e00
ianfi96 <ilfischer@uc.cl>
2021-04-11 16:30:41 -0400

    final commit

--- app/controllers/season_controller.rb
+++ app/controllers/season_controller.rb
@@ -1,13 +1,13 @@
 class SeasonController < ApplicationController
 
     def index
-        season = params[:season]
+        @season = params[:season]
         series = CGI.escape("#{params[:series]}")
         list_of_episodes = HTTP.get("https://tarea-1-breaking-bad.herokuapp.com/api/episodes?series=#{series}").to_s
         parsed_episodes = JSON.parse(list_of_episodes)
         @parsed_season_episodes = []
         parsed_episodes.each do |episode|
-            if episode["season"] == season
+            if episode["season"] == @season
                 @parsed_season_episodes.append(episode)
             end
         end
